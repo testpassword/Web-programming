83d7fa51674332f2acd7f70009cba2086df4f987
Artemy <kulbakolearn@mail.ru>
2019-12-10 00:01:00 +0300

    фронтенд: CheckButton имеет несколько цветов на выбор; добавлена кнопка регистрации; созданы прототипы всех методов в Validator.vue и Accessor.vue; Axios-объект настроен на относительные пути; бэкенд: создан конфигурационный файл базы данных

--- lab4-12.11.19/hominem/src/main.js
+++ lab4-12.11.19/hominem/src/main.js
@@ -4,7 +4,7 @@ import router from "@/router";
 import Axios from "axios";
 
 Vue.config.productionTip = false;
-Vue.prototype.$axios = Axios;
+Vue.prototype.$axios = Axios.create({baseURL: "http://localhost:8080"});
 
 new Vue({
   router, render: h => h(App),
